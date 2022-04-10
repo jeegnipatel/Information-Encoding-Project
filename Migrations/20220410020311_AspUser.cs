da294e4ec30e8643b37f84ad9762ea9a3116714f
Jeegni Patel <101203301+jeegnipatel@users.noreply.github.com>
2022-04-09 23:00:18 -0400

    Migrations

---
+++ Migrations/20220410020311_AspUser.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManagement.MVC.Migrations
+{
+    public partial class AspUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                schema: "Identity",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                schema: "Identity",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                schema: "Identity",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                schema: "Identity",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                schema: "Identity",
+                table: "User");
+        }
+    }
+}
